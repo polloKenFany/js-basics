66783103d61b4c5fe9f755a9b0b2ae2b11314b19
polloKenFany <estef.fig@gmail.com>
2021-08-25 13:27:38 -0500

    Access system refactor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-console.log("Bienvenidos al sitio de Volaris");
-console.log("Para comprar nuestros boletos de avión debes ser mayor de 18 años");
+var nombre = "Pablo";
+console.log(nombre + " Bienvenide al sitio de Volaris");
+console.log("Estimade " + nombre + " para comprar nuestros boletos de avión debes ser mayor de 18 años");
 var edad = 5;
 if (edad>18) {
-    console.log("Puedes comprar");
+    console.log("Felicidades " + nombre + " ya puedes comprar tus boletos en línea!!! Buen viaje.");
 } else {
-    console.log("NO Puedes comprar");
+    console.log("Lo sentimos " + nombre + " no puedes acceder a Volaris por que tienes " + edad + " años, necesitas autorización de un adulto :(");
 }
