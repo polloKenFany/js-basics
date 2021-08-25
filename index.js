3767f1b963073e55662c5007d3c63501873cf2f6
polloKenFany <estef.fig@gmail.com>
2021-08-25 14:06:43 -0500

    Aerolínea gatuna

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var nombre = "Pablo";
-console.log(nombre + " Bienvenide al sitio de Volaris");
+console.log(nombre + " Bienvenide al sitio de Aerolínea gatuna");
 console.log("Estimade " + nombre + " para comprar nuestros boletos de avión debes ser mayor de 18 años");
 var edad = 5;
 if (edad>18) {
     console.log("Felicidades " + nombre + " ya puedes comprar tus boletos en línea!!! Buen viaje.");
 } else {
-    console.log("Lo sentimos " + nombre + " no puedes acceder a Volaris por que tienes " + edad + " años, necesitas autorización de un adulto :(");
+    console.log("Lo sentimos " + nombre + " no puedes acceder a Aerolínea gatuna por que tienes " + edad + " años, necesitas autorización de un adulto :(");
 }
